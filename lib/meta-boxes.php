2635863804e420ca196a2bed5d803f2aa8d9d431
mikeyrayvon <mikey@interglobal.vision>
2018-12-07 16:47:10 -0600

    add home metabox

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -39,5 +39,45 @@ function igv_cmb_metaboxes() {
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
    */
 
+	// HOME
+  $home_page = get_page_by_path('home');
+
+  if (!empty($home_page) ) {
+
+    $home_metabox = new_cmb2_box( array(
+      'id'            => $prefix . 'home_metabox',
+      'title'         => esc_html__( 'Options', 'cmb2' ),
+      'object_types'  => array( 'page' ), // Post type
+      'show_on'      => array( 'key' => 'id', 'value' => array($home_page->ID) ),
+    ) );
+
+    $home_metabox->add_field( array(
+  		'name' => esc_html__( 'Location', 'cmb2' ),
+  		'id'   => $prefix . 'home_location',
+  		'type' => 'wysiwyg',
+      'options' => array(
+  	    'wpautop' => false, // use wpautop?
+  	    'media_buttons' => false, // show insert/upload button(s)
+  	    'textarea_rows' => 5, // rows="..."
+  	    'teeny' => true, // output the minimal editor config used in Press This
+  	    'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+    	),
+  	) );
+
+    $home_metabox->add_field( array(
+  		'name' => esc_html__( 'Press', 'cmb2' ),
+  		'id'   => $prefix . 'home_press',
+  		'type' => 'wysiwyg',
+      'options' => array(
+  	    'wpautop' => false, // use wpautop?
+  	    'media_buttons' => false, // show insert/upload button(s)
+  	    'textarea_rows' => 5, // rows="..."
+  	    'teeny' => true, // output the minimal editor config used in Press This
+  	    'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+    	),
+  	) );
+
+  }
+
 }
 ?>
