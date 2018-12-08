40fd0a54fafd1190b8e15b9c1d467a7ab3a03ba3
mikeyrayvon <mikey@interglobal.vision>
2018-12-07 19:08:55 -0600

    restore metaboxes and post type

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -39,5 +39,64 @@ function igv_cmb_metaboxes() {
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
    */
 
+	// HOME
+  $home_page = get_page_by_path('home');
+
+  if (!empty($home_page) ) {
+
+    $home_metabox = new_cmb2_box( array(
+      'id'            => $prefix . 'home_metabox',
+      'title'         => esc_html__( 'Fields', 'cmb2' ),
+      'object_types'  => array( 'page' ), // Post type
+      'show_on'      => array( 'key' => 'id', 'value' => array($home_page->ID) ),
+    ) );
+
+    $home_metabox->add_field( array(
+  		'name' => esc_html__( 'Location', 'cmb2' ),
+  		'id'   => $prefix . 'home_location',
+  		'type' => 'wysiwyg',
+      'options' => array(
+  	    'media_buttons' => false, // show insert/upload button(s)
+  	    'textarea_rows' => 10, // rows="..."
+    	),
+  	) );
+
+    $home_metabox->add_field( array(
+  		'name' => esc_html__( 'Press', 'cmb2' ),
+  		'id'   => $prefix . 'home_press',
+  		'type' => 'wysiwyg',
+      'options' => array(
+  	    'media_buttons' => false, // show insert/upload button(s)
+  	    'textarea_rows' => 10, // rows="..."
+    	),
+  	) );
+
+  }
+
+  // FAIR
+  $fair_metabox = new_cmb2_box( array(
+    'id'            => $prefix . 'fair_metabox',
+    'title'         => esc_html__( 'Fields', 'cmb2' ),
+    'object_types'  => array( 'fair' ), // Post type
+  ) );
+
+  $fair_metabox->add_field( array(
+    'name' => esc_html__( 'Year', 'cmb2' ),
+    'id'   => $prefix . 'fair_year',
+    'type' => 'text',
+  ) );
+
+  $fair_metabox->add_field( array(
+    'name' => esc_html__( 'Participants', 'cmb2' ),
+    'id'   => $prefix . 'fair_participants',
+    'type' => 'wysiwyg',
+    'options' => array(
+      'media_buttons' => false, // show insert/upload button(s)
+      'textarea_rows' => 20, // rows="..."
+    ),
+  ) );
+
+
+
 }
 ?>
