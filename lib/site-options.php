76caa7e159cb1b43aeffe5a293f92cd6c159c804
mikeyrayvon <mikey@interglobal.vision>
2018-12-07 19:10:09 -0600

    add email and logo site options

--- lib/site-options.php
+++ lib/site-options.php
@@ -4,40 +4,6 @@ add_action( 'cmb2_admin_init', 'igv_register_theme_options_metabox' );
 function igv_register_theme_options_metabox() {
   $prefix = '_igv_';
 
-  $boiler_options = new_cmb2_box( array(
-    'id'           => $prefix . 'boiler_options_page',
-    'title'        => esc_html__( 'Boiler Options', 'cmb2' ),
-    'object_types' => array( 'options-page' ),
-    /*
-     * The following parameters are specific to the options-page box
-     * Several of these parameters are passed along to add_menu_page()/add_submenu_page().
-     */
-    'option_key'      => $prefix . 'boiler_options', // The option key and admin menu page slug.
-    'icon_url'        => 'dashicons-layout', // Menu icon. Only applicable if 'parent_slug' is left empty.
-    // 'menu_title'      => esc_html__( 'Options', 'cmb2' ), // Falls back to 'title' (above).
-    // 'parent_slug'     => 'themes.php', // Make options page a submenu item of the themes menu.
-    'capability'      => 'manage_options', // Cap required to view options-page.
-    // 'position'        => 1, // Menu position. Only applicable if 'parent_slug' is left empty.
-    // 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
-    // 'display_cb'      => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
-    'save_button'     => esc_html__( 'Boil me baby', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
-  ) );
-
-  $boiler_options->add_field( array(
-    'name'    => esc_html__( 'THE TITLE', 'cmb2' ),
-    'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-    'id'      => 'title',
-    'type'    => 'title',
-  ) );
-
-  $boiler_options->add_field( array(
-    'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
-    'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-    'id'      => 'bg_color',
-    'type'    => 'colorpicker',
-    'default' => '#ffffff',
-  ) );
-
   // Site options for general data
 
   $site_options = new_cmb2_box( array(
@@ -56,6 +22,20 @@ function igv_register_theme_options_metabox() {
     // 'admin_menu_hook' => 'network_admin_menu', // 'network_admin_menu' to add network-level options page.
     // 'display_cb'      => false, // Override the options-page form output (CMB2_Hookup::options_page_output()).
     // 'save_button'     => esc_html__( 'Save Theme Options', 'cmb2' ), // The text for the options-page save button. Defaults to 'Save'.
+  ) );
+
+  $site_options->add_field( array(
+    'name'    => esc_html__( 'OPaf Logo', 'cmb2' ),
+    'desc'    => esc_html__( '', 'cmb2' ),
+    'id'      => 'opaf_logo',
+    'type'    => 'file',
+  ) );
+
+  $site_options->add_field( array(
+    'name'    => esc_html__( 'Contact Email', 'cmb2' ),
+    'desc'    => esc_html__( '', 'cmb2' ),
+    'id'      => 'contact_email',
+    'type'    => 'text',
   ) );
 
   // Social Media variables
